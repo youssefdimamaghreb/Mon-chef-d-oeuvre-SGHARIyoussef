46c15ae465609a1f7ee784cdb9b33c128d979df6
youssefdimamaghreb <youssefsg09@gmail.com>
2025-11-13 15:23:49 +0000

    feat: Implémentation du toggle dark move

--- script.js
+++ script.js
@@ -11,3 +11,17 @@ if (titre) {
     titre.textContent = "Titre cliqué !";
   });
 }
+// --- LOGIQUE DU DARK MODE ---
+// 1. CIBLER
+const themeButton = document.querySelector('#theme-toggle');
+
+// On vérifie si le bouton existe sur la page actuelle
+if (themeButton) {
+  // 2. ÉCOUTER
+  themeButton.addEventListener('click', () => {
+    // 3. MANIPULER
+    // On ajoute/retire la classe 'dark-mode' sur le body
+    document.body.classList.toggle('dark-mode');
+  });
+}
+
